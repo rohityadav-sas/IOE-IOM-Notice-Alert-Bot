47bdd922080fdfd51f418e0bb6269427963dd1d3
Rohit Yadav <rohityadav.se@gmail.com>
2024-09-16 20:45:48 +0545

    Auto-commit: Changes detected

--- src/utils/auto-push.js
+++ src/utils/auto-push.js
@@ -7,7 +7,6 @@ const git = simpleGit(repoPath);
 async function pushChanges() {
     try {
         const status = await git.status();
-        console.log(status);
         if (status.files.length === 0) {
             console.log('No changes detected');
             return;
