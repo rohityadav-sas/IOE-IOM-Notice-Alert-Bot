09ada0afa0d7495e3a3dea51642186e4dba053de
Rohit Yadav <rohityadav.se@gmail.com>
2024-09-18 22:44:03 +0545

    hosting using another githun

--- src/utils/botManager.js
+++ src/utils/botManager.js
@@ -20,7 +20,7 @@ async function botOnStart(bot, chatIdsPath, college) {
             await bot.sendMessage(msg.chat.id, 'View logs:', {
                 reply_markup: {
                     inline_keyboard: [[
-                        { text: 'Logs', web_app: { url: `https://ioe-iom-notice-alert-bot.onrender.com/logs/?token=${token}`, hide_url: true } }
+                        { text: 'Logs', web_app: { url: `https://ioe-iom-notice-alert-bot-bkvn.onrender.com/logs/?token=${token}`, hide_url: true } }
                     ]]
                 }
             })
