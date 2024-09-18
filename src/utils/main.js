089379c2b784abd9b15a15ce28ca76ebda5cbdf4
Rohit Yadav <rohityadav.se@gmail.com>
2024-09-18 22:25:44 +0545

    forgot to change interval

--- src/utils/main.js
+++ src/utils/main.js
@@ -12,7 +12,7 @@ async function handleBot(botToken, chatIdsPath, savedNoticesPath, sendNoticeFn, 
         sendNoticeFn(bot);
         setInterval(async () => {
             await sendNoticeFn(bot);
-        }, 10000);
+        }, 5 * 60 * 1000);
     } catch (error) {
         console.error(`Error with ${botName}:`, error);
     }
