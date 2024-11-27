5ecb08dd9f9a0c0e7758b30a7f632a749ead5a80
Rohit <rohityadav.se@gmail.com>
2024-11-27 18:44:20 +0545

    Scheduled commit

--- src/utils/noticeManager.js
+++ src/utils/noticeManager.js
@@ -40,13 +40,18 @@ async function checkForNewNotices(currentNotices, savedNotices, filePath) {
 async function handleNotices(fetchCurrentNotices, savedNoticesPath) {
 	try {
 		const savedNotices = await fetchSavedNotices(savedNoticesPath);
+		console.log(`Saved Notices: `, savedNotices);
 		const currentNotices = await fetchCurrentNotices;
+		console.log(`Current Notices: `, currentNotices);
 		const newNotices = await checkForNewNotices(
 			currentNotices,
 			savedNotices,
 			savedNoticesPath
 		);
+		console.log(`New Notices: `, newNotices);
 		const allNotices = [...newNotices, ...savedNotices];
+		console.log(`All Notices: `, allNotices);
+
 		if (allNotices.length > 5) {
 			saveNotices(allNotices.slice(0, 5), savedNoticesPath);
 		} else {
--- src/utils/noticeSender.js
+++ src/utils/noticeSender.js
@@ -5,72 +5,92 @@ const { fetchChatIds, removeChatId } = require('./chatIdManager');
 const { log } = require('./logger');
 const paths = require('./filePaths');
 
-async function processNotices(bot, fetchCurrentNotices, savedNoticesPath, chatIdsPath) {
-    try {
-        const newNotices = await handleNotices(fetchCurrentNotices, savedNoticesPath);
-        await sendNotices(bot, newNotices, chatIdsPath);
-    } catch (error) {
-        console.error(`Error processing notices: ${error.message}`);
-    }
+async function processNotices(
+	bot,
+	fetchCurrentNotices,
+	savedNoticesPath,
+	chatIdsPath
+) {
+	try {
+		const newNotices = await handleNotices(
+			fetchCurrentNotices,
+			savedNoticesPath
+		);
+		await sendNotices(bot, newNotices, chatIdsPath);
+	} catch (error) {
+		console.error(`Error processing notices: ${error.message}`);
+	}
 }
 
 async function sendNotices(bot, newNotices, chatIdsPath) {
-    try {
-        if (newNotices.length > 0) {
-            const chatIds = await fetchChatIds(chatIdsPath);
-            await sendMessagesToChatIds(bot, chatIds, newNotices, chatIdsPath);
-            let logMessage = '';
-            newNotices.forEach(notice => {
-                logMessage += `New notice received: ${notice.Description}`;
-            });
-            await log(logMessage);
-        }
-    } catch (error) {
-        console.error(`Error sending notices: ${error.message}`);
-    }
+	try {
+		if (newNotices.length > 0) {
+			const chatIds = await fetchChatIds(chatIdsPath);
+			await sendMessagesToChatIds(bot, chatIds, newNotices, chatIdsPath);
+			let logMessage = '';
+			newNotices.forEach((notice) => {
+				logMessage += `New notice received: ${notice.Description}`;
+			});
+			await log(logMessage);
+		}
+	} catch (error) {
+		console.error(`Error sending notices: ${error.message}`);
+	}
 }
 
 async function sendMessagesToChatIds(bot, chatIds, notices, chatIdsPath) {
-    for (const chatId of chatIds) {
-        for (let j = notices.length - 1; j >= 0; j--) {
-            const notice = notices[j];
-            const message = formatMessage(notice);
-            try { await bot.sendMessage(chatId, message, { parse_mode: 'HTML' }); }
-            catch (error) {
-                if (error.response && error.response.statusCode === 403) {
-                    await removeChatId(chatId, chatIdsPath);
-                    await log(`User with chatId ${chatId} has blocked the bot.`, 'red');
-                }
-                else {
-                    await log(`Error sending message to ${chatId}: ${error.message}`, 'red');
-                }
-            }
-        }
-    }
+	for (const chatId of chatIds) {
+		for (let j = notices.length - 1; j >= 0; j--) {
+			const notice = notices[j];
+			const message = formatMessage(notice);
+			try {
+				await bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
+			} catch (error) {
+				if (error.response && error.response.statusCode === 403) {
+					await removeChatId(chatId, chatIdsPath);
+					await log(`User with chatId ${chatId} has blocked the bot.`, 'red');
+				} else {
+					await log(
+						`Error sending message to ${chatId}: ${error.message}`,
+						'red'
+					);
+				}
+			}
+		}
+	}
 }
 
 function formatMessage(notice) {
-    const { Date: date, Description: description, Url: url } = notice;
-    return `<b>📅  Date: </b><u><b>${date}</b></u>\n\n<b>📝  Notice:</b>\n<b><i>${description}</i></b>\n\n<a href="${url}">🔗 Read more</a>\n\n<i>Stay updated for more important news!</i> 💡`;
+	const { Date: date, Description: description, Url: url } = notice;
+	return `<b>📅  Date: </b><u><b>${date}</b></u>\n\n<b>📝  Notice:</b>\n<b><i>${description}</i></b>\n\n<a href="${url}">🔗 Read more</a>\n\n<i>Stay updated for more important news!</i> 💡`;
 }
 
 async function sendNoticeIOE(bot) {
-    let noticePaths = {
-        'exam': paths.IOEExamNoticesPath,
-        'entrance': paths.IOEEntranceNoticesPath,
-        'official': paths.IOEOfficialPageNoticesPath,
-        'admission': paths.IOEAdmissionNoticesPath
-    }
-    await Promise.all(
-        Object.keys(noticePaths)
-            .map(async (key) => {
-                await processNotices(bot, fetchCurrentNoticesIOE(key), noticePaths[key], paths.chatIdsPathIOE);
-            })
-    );
+	let noticePaths = {
+		// 'exam': paths.IOEExamNoticesPath,
+		// 'entrance': paths.IOEEntranceNoticesPath,
+		official: paths.IOEOfficialPageNoticesPath
+		// 'admission': paths.IOEAdmissionNoticesPath
+	};
+	await Promise.all(
+		Object.keys(noticePaths).map(async (key) => {
+			await processNotices(
+				bot,
+				fetchCurrentNoticesIOE(key),
+				noticePaths[key],
+				paths.chatIdsPathIOE
+			);
+		})
+	);
 }
 
 async function sendNoticeIOM(bot) {
-    await processNotices(bot, fetchCurrentNoticesIOM(), paths.IOMExamNoticesPath, paths.chatIdsPathIOM);
+	await processNotices(
+		bot,
+		fetchCurrentNoticesIOM(),
+		paths.IOMExamNoticesPath,
+		paths.chatIdsPathIOM
+	);
 }
 
 module.exports = { sendNoticeIOE, sendNoticeIOM, sendMessagesToChatIds };
