c4327a7306b8bac29141894c23b5edd6854c0bf4
Rohit <rohityadav.se@gmail.com>
2024-11-27 18:32:42 +0545

    Scheduled commit

--- src/utils/noticeManager.js
+++ src/utils/noticeManager.js
@@ -1,47 +1,62 @@
 const fs = require('fs');
 
 async function fetchSavedNotices(filePath) {
-    try {
-        if (!fs.existsSync(filePath)) { fs.writeFileSync(filePath, '[]') };
-        const data = fs.readFileSync(filePath, 'utf-8');
-        if (data.length === 0) { return [] };
-        return JSON.parse(data);
-    }
-    catch (err) {
-        throw (err);
-    }
+	try {
+		if (!fs.existsSync(filePath)) {
+			fs.writeFileSync(filePath, '[]');
+		}
+		const data = fs.readFileSync(filePath, 'utf-8');
+		if (data.length === 0) {
+			return [];
+		}
+		return JSON.parse(data);
+	} catch (err) {
+		throw err;
+	}
 }
 
 async function saveNotices(notices, filePath) {
-    fs.writeFileSync(filePath, JSON.stringify(notices, null, 2));
+	fs.writeFileSync(filePath, JSON.stringify(notices, null, 2));
 }
 
 async function checkForNewNotices(currentNotices, savedNotices, filePath) {
-    const newNotices = currentNotices.filter((notice) =>
-        !savedNotices.some(savedNotice =>
-            savedNotice.Date === notice.Date &&
-            savedNotice.Description === notice.Description
-        ));
-    if (newNotices.length > 0) {
-        savedNotices.unshift(...newNotices);
-        saveNotices(savedNotices, filePath);
-        return newNotices;
-    }
-    else {
-        return [];
-    }
+	const newNotices = currentNotices.filter(
+		(notice) =>
+			!savedNotices.some(
+				(savedNotice) =>
+					savedNotice.Date === notice.Date &&
+					savedNotice.Description === notice.Description
+			)
+	);
+	if (newNotices.length > 0) {
+		savedNotices.unshift(...newNotices);
+		saveNotices(savedNotices, filePath);
+		return newNotices;
+	} else {
+		return [];
+	}
 }
 
 async function handleNotices(fetchCurrentNotices, savedNoticesPath) {
-    try {
-        const savedNotices = await fetchSavedNotices(savedNoticesPath);
-        const currentNotices = await fetchCurrentNotices;
-        const newNotices = await checkForNewNotices(currentNotices, savedNotices, savedNoticesPath);
-        return newNotices;
-    } catch (error) {
-        console.error(`Error fetching notices: ${error.message}`);
-        throw error;
-    }
+	try {
+		const savedNotices = await fetchSavedNotices(savedNoticesPath);
+		const currentNotices = await fetchCurrentNotices;
+		const newNotices = await checkForNewNotices(
+			currentNotices,
+			savedNotices,
+			savedNoticesPath
+		);
+		const allNotices = [...newNotices, ...savedNotices];
+		if (allNotices.length > 5) {
+			saveNotices(allNotices.slice(0, 5), savedNoticesPath);
+		} else {
+			saveNotices(allNotices, savedNoticesPath);
+		}
+		return newNotices;
+	} catch (error) {
+		console.error(`Error fetching notices: ${error.message}`);
+		throw error;
+	}
 }
 
 module.exports = { fetchSavedNotices, checkForNewNotices, handleNotices };
