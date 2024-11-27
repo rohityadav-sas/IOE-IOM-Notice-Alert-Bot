bc77730dc45261bf6b7fe4e44f0854d1fa9d3c44
Rohit <rohityadav.se@gmail.com>
2024-11-27 18:18:06 +0545

    Scheduled commit

--- getChatDetails.js
+++ getChatDetails.js
@@ -1,15 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN_IOE, { polling: true });
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN_IOE, {
+	polling: true
+});
 
 const getChatDetails = async (chatId) => {
-    try {
-        const chat = await bot.getChat(chatId);
-        console.log(chat);
-    } catch (error) {
-        console.error('Error getting chat details:', error);
-    }
+	try {
+		const chat = await bot.getChat(chatId);
+		console.log(chat);
+	} catch (error) {
+		console.error('Error getting chat details:', error);
+	}
 };
 
-getChatDetails('1012711232');
+getChatDetails('1988702309');
--- src/utils/main.js
+++ src/utils/main.js
@@ -5,38 +5,60 @@ const { log } = require('./logger');
 const paths = require('./filePaths');
 const { pushChanges } = require('./gitHelper');
 
-async function handleBot(botToken, chatIdsPath, savedNoticesPath, sendNoticeFn, botName) {
-    const bot = new TelegramBot(botToken, { polling: true });
-    try {
-        await botOnStart(bot, chatIdsPath, botName);
-        await botOnCallback(bot, chatIdsPath, botName, savedNoticesPath);
-        sendNoticeFn(bot);
-        setInterval(async () => {
-            await sendNoticeFn(bot);
-        }, 5 * 1000 * 60);
-        if (botName === 'IOE') {
-            setInterval(async () => {
-                await pushChanges('Scheduled commit', bot);
-            }, 6 * 60 * 60 * 1000);
-        }
-    } catch (error) {
-        console.error(`Error with ${botName}:`, error);
-    }
+async function handleBot(
+	botToken,
+	chatIdsPath,
+	savedNoticesPath,
+	sendNoticeFn,
+	botName
+) {
+	const bot = new TelegramBot(botToken, { polling: true });
+	try {
+		await botOnStart(bot, chatIdsPath, botName);
+		await botOnCallback(bot, chatIdsPath, botName, savedNoticesPath);
+		sendNoticeFn(bot);
+		setInterval(async () => {
+			await sendNoticeFn(bot);
+		}, 10000);
+		if (botName === 'IOE') {
+			setInterval(async () => {
+				await pushChanges('Scheduled commit', bot);
+			}, 10000);
+		}
+	} catch (error) {
+		console.error(`Error with ${botName}:`, error);
+	}
 }
 
 async function main() {
-    try {
-        const IOENoticesPath = [paths.IOEExamNoticesPath, paths.IOEEntranceNoticesPath, paths.IOEOfficialPageNoticesPath, paths.IOEAdmissionNoticesPath];
-        const IOMNoticesPath = [paths.IOMExamNoticesPath];
-        await Promise.all([
-            handleBot(process.env.TELEGRAM_BOT_TOKEN_IOE, paths.chatIdsPathIOE, IOENoticesPath, sendNoticeIOE, 'IOE'),
-            handleBot(process.env.TELEGRAM_BOT_TOKEN_IOM, paths.chatIdsPathIOM, IOMNoticesPath, sendNoticeIOM, 'IOM')
-        ]);
-
-    } catch (error) {
-        console.error('An error occurred while running the bots:', error);
-        await log(`Error with bot: ${error.message}`);
-    }
+	try {
+		const IOENoticesPath = [
+			paths.IOEExamNoticesPath,
+			paths.IOEEntranceNoticesPath,
+			paths.IOEOfficialPageNoticesPath,
+			paths.IOEAdmissionNoticesPath
+		];
+		const IOMNoticesPath = [paths.IOMExamNoticesPath];
+		await Promise.all([
+			handleBot(
+				process.env.TELEGRAM_BOT_TOKEN_IOE,
+				paths.chatIdsPathIOE,
+				IOENoticesPath,
+				sendNoticeIOE,
+				'IOE'
+			),
+			handleBot(
+				process.env.TELEGRAM_BOT_TOKEN_IOM,
+				paths.chatIdsPathIOM,
+				IOMNoticesPath,
+				sendNoticeIOM,
+				'IOM'
+			)
+		]);
+	} catch (error) {
+		console.error('An error occurred while running the bots:', error);
+		await log(`Error with bot: ${error.message}`);
+	}
 }
 
 module.exports = { main };
